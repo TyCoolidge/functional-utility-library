4a02a5fe01f28edc28d5eb94a118815104c671d7
TyCoolidge <62319016+TyCoolidge@users.noreply.github.com>
2020-05-23 14:11:06 -0700

    added more functional programming

--- functions.js
+++ functions.js
@@ -4,6 +4,7 @@ function filteringNumbers(input) {
    var numbersToDivide = [44, 16, 12, 64, 9, 33, 36, 15, input]; //global
 
    Array.prototype.numFilter = function (divideNumbers) {
+      // divideNumbers is the callback that triggers line 21
       //;/ filter that carries the function of 'divideByFour'
       var arrayFactors = []; /// empty array that results will be pushed into
 
@@ -24,3 +25,36 @@ function filteringNumbers(input) {
    });
    return divideByFour;
 } //TODO try to add two inputs
+
+///Return Part of an Array Using the slice Method
+function sliceArray(inputBeginSlice, inputEndSlice) {
+   // It can take two arguments, the first gives the index of where to begin the slice, the second is the index for where to end the slice
+   var inputGuards = ["Curry", " Magic", " Kyrie", " Stockton", " Nash"];
+   var newArray = inputGuards.slice(inputBeginSlice, inputEndSlice);
+   return newArray;
+}
+
+//Remove Elements from an Array Using slice Instead of splice
+function sliceNotSplice(input) {
+   var inputConsoles = ["PS4", " Xbox One", " Switch", " Wii", " Dreamcast"];
+
+   return inputConsoles.slice(0, input);
+}
+
+///testing Combine Two Arrays Using the concat Method
+function userConcat(concatInput) {
+   if (concatInput === 1) {
+      return legends.concat(option1);
+   } else if (concatInput === 2) {
+      return legends.concat(option2);
+   } else if (concatInput === 3) {
+      return legends.concat(option3);
+   } else {
+      return legends;
+   }
+}
+var legends = ["MJ", " Kobe", " Lebron"];
+var option1 = [" Kareem", " Wilt"];
+var option2 = [" Shaq", " Bird"];
+var option3 = [" Magic", " Pippen"];
+//////////////////////////////////////////////////
